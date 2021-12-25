08e5736903f61d839ebb778c30c42943952712e5
Iskandar <30454668+Iskandar-S@users.noreply.github.com>
2021-12-25 12:59:27 -0500

    created 'messages' object to store message components arrays

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 console.log('hello');
+
+  const messages = {
+  meta_character: ['a unicorn', 'a cyborg', 'a zombie', 'Elvis', 'a doge', 'Buzz Lightyear', 'Yoshimitsu', 'Mario', 'Homer Simpson', 'Che Guevara', 'Lizardman', 'Joe Rogan', 'Gandalf', 'Trinity', 'Darth Vader', 'Mufasa', 'Elon', 'Octocat', 'Sakura', 'Pikachu', 'Hermione', 'Wendy', 'Putin', 'AOC', 'Wonder Woman', 'Psylocke', 'Mulan', 'Daenerys', 'Moana'],
+
+};
